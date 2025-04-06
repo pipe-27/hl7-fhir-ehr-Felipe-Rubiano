ee1efad5efeb901d874d2eb9fc767be11013b0ae
pipe-27 <feliperubianoestupinan@gmail.com>
2025-04-06 00:12:20 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb://felipe:<db_password>@sampleinformationservic-shard-00-00.fkbk7.mongodb.net:27017,sampleinformationservic-shard-00-01.fkbk7.mongodb.net:27017,sampleinformationservic-shard-00-02.fkbk7.mongodb.net:27017/?replicaSet=atlas-3q3dfi-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=SampleInformationService"
+    uri = "mongodb://felipe:hl7-fhir-ehr-Felipe-Rubianosampleinformationservic-shard-00-00.fkbk7.mongodb.net:27017,sampleinformationservic-shard-00-01.fkbk7.mongodb.net:27017,sampleinformationservic-shard-00-02.fkbk7.mongodb.net:27017/?replicaSet=atlas-3q3dfi-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=SampleInformationService"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
