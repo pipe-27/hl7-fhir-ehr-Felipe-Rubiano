9076167e6cc85bd4643e2f912a2824a0cba0ebf3
pipe-27 <feliperubianoestupinan@gmail.com>
2025-04-06 01:50:22 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,16 +10,15 @@ from app.controlador.PatientCrud import (
 
 app = FastAPI()
 
-# Configurar CORS
+# CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # En producción, reemplaza "*" por los dominios permitidos
+    allow_origins=["*"],  # Puedes reemplazar "*" por el dominio exacto de tu frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Obtener paciente por ID
 @app.get("/patient/{patient_id}", response_model=dict)
 async def get_patient_by_id(patient_id: str):
     status, patient = GetPatientById(patient_id)
@@ -30,7 +29,6 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
-# Obtener paciente por identificador
 @app.get("/patient", response_model=dict)
 async def get_patient_by_identifier(system: str, value: str):
     print("received", system, value)
@@ -42,7 +40,15 @@ async def get_patient_by_identifier(system: str, value: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
-# Obtener solicitud de servicio por ID
+@app.post("/patient", response_model=dict)
+async def add_patient(request: Request):
+    new_patient_dict = dict(await request.json())
+    status, patient_id = WritePatient(new_patient_dict)
+    if status == 'success':
+        return {"_id": patient_id}
+    else:
+        raise HTTPException(status_code=500, detail=f"Validating error: {status}")
+
 @app.get("/service-request/{service_request_id}", response_model=dict)
 async def get_service_request(service_request_id: str):
     service_request = read_service_request(service_request_id)
@@ -51,17 +57,6 @@ async def get_service_request(service_request_id: str):
     else:
         raise HTTPException(status_code=404, detail="Solicitud de servicio no encontrada")
 
-# Agregar paciente
-@app.post("/patient", response_model=dict)
-async def add_patient(request: Request):
-    new_patient_dict = await request.json()
-    status, patient_id = WritePatient(new_patient_dict)
-    if status == 'success':
-        return {"_id": patient_id}
-    else:
-        raise HTTPException(status_code=500, detail=f"Validating error: {status}")
-
-# Agregar solicitud de servicio
 @app.post("/service-request", response_model=dict)
 async def add_service_request(request: Request):
     service_request_data = await request.json()
@@ -71,7 +66,8 @@ async def add_service_request(request: Request):
     else:
         raise HTTPException(status_code=500, detail=f"Error al registrar la solicitud: {status}")
 
-# Solo ejecuta el servidor si se corre directamente este archivo
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
