9d3afee39fa91f07becd5cc2e2905ca55d0e388d
pipe-27 <feliperubianoestupinan@gmail.com>
2025-04-05 21:56:39 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient
+from app.controlador.PatientCrud import GetPatientById,GetPatientByIdentifier,WritePatient,read_service_request,WriteServiceRequest
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hl7-patient-write.onrender.com"],  # Permitir solo este dominio
+    allow_origins=["https://hl7-patient-write-karol904.onrender.com"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
@@ -23,6 +23,26 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
+@app.get("/patient", response_model=dict)
+async def get_patient_by_identifier(system: str, value: str):
+    print("received", system, value)
+    status, patient = GetPatientByIdentifier(system, value)
+    if status == 'success':
+        return patient
+    elif status == 'notFound':
+        raise HTTPException(status_code=404, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
+@app.get("/service-request/{service_request_id}", response_model=dict)
+async def get_service_request(service_request_id: str):
+    # Llama a la función auxiliar que obtiene la solicitud de servicio
+    service_request = read_service_request(service_request_id)
+    if service_request:
+        return service_request
+    else:
+        raise HTTPException(status_code=404, detail="Solicitud de servicio no encontrada")
+
 
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
@@ -36,3 +56,16 @@ async def add_patient(request: Request):
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+@app.post("/service-request", response_model=dict)
+async def add_service_request(request: Request):
+    # Obtiene el JSON enviado en la solicitud POST
+    service_request_data = await request.json()
+    
+    # Llama a la función que inserta la solicitud en la base de datos
+    status, service_request_id = WriteServiceRequest(service_request_data)
+    
+    if status == "success":
+        return {"_id": service_request_id}
+    else:
+        raise HTTPException(status_code=500, detail=f"Error al registrar la solicitud: {status}")
