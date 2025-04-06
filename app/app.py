21470534de78d6015bfdd612e424a6307c18e97b
pipe-27 <feliperubianoestupinan@gmail.com>
2025-04-05 23:56:39 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hl7-patient-write-karol904.onrender.com"],  # Permitir solo este dominio
+    allow_origins=["*"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
